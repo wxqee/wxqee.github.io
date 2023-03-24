09330912cd5ae3a56a2cff0da8c482526855cbc1
xq21 <xq21@vip.qq.com>
2023-03-25 01:29:50 +0800

    百度统计

---
+++ serve.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Jekyll Docs -- https://jekyllrb.com/docs/
+# Working with Jekyll Themes -- https://jekyllrb.com/docs/themes/#overriding-theme-defaults
+
+# for Jekyll's default theme
+# > bundle info --path minima
+
+bundle exec jekyll serve --livereload
